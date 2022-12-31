c856c23455ad5b27928ff9cdec895640e5e7fb74
desvafitranda <89762116+desvafitranda@users.noreply.github.com>
2022-12-31 16:38:53 +0700

    Desva commit

---
+++ bakery-app/bakery/src/main/java/com/uaspbo/interface/FillingInterface.java
@@ -0,0 +1,8 @@
+package com.uaspbo.Interface;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public interface FillingInterface {
+  Map<String, Double[]> totalFilling = new HashMap<String, Double[]>();
+}
---
+++ bakery-app/bakery/src/main/java/com/uaspbo/interface/ToppingInterface.java
@@ -0,0 +1,9 @@
+package com.uaspbo.Interface;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public interface ToppingInterface {
+  Map<String, Double[]> totalTopping = new HashMap<String, Double[]>();
+
+}
