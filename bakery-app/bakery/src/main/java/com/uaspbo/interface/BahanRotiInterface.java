7121b6082b3b69c55630dadcebbd7d2047596771
PangestuFarhanIlahi <89761603+PangestuFarhanIlahi@users.noreply.github.com>
2022-12-31 16:37:55 +0700

    Pangestu Commit2

---
+++ bakery-app/bakery/src/main/java/com/uaspbo/interface/BahanRotiInterface.java
@@ -0,0 +1,13 @@
+package com.uaspbo.Interface;
+
+public interface BahanRotiInterface {
+  // nama bahan = { BeratKemasan, Harga }
+  Double[] terigu = { 1000.0, 40000.0 };
+  Double[] gula = { 1000.0, 25000.0 };
+  Double[] butter = { 500.0, 23000.0 };
+  Double[] ragi = { 11.0, 5000.0 };
+  Double[] susuBubuk = { 1000.0, 39000.0 };
+  Double[] susuCair = { 1000.0, 24000.0 };
+  Double[] telur = { 1000.0, 23000.0 };
+  Double[] esBatu = {1000.0, 2000.0};
+}
