2fc20aaf782e9942a116f30d5091b7bff20c0a35
PangestuFarhanIlahi <89761603+PangestuFarhanIlahi@users.noreply.github.com>
2022-12-31 16:36:04 +0700

    Pangestu Commit

---
+++ bakery-app/bakery/src/main/java/com/uaspbo/interface/BahanVariantInterface.java
@@ -0,0 +1,12 @@
+package com.uaspbo.Interface;
+
+public interface BahanVariantInterface {
+  // nama bahan = { BeratKemasan, Harga }
+  Double[] keju = { 250.0, 31000.0 };
+  Double[] coklat = { 500.0, 29000.0 };
+  Double[] vanilla = { 500.0, 30000.0 };
+  Double[] redBean = { 500.0, 25000.0 };
+  Double[] sosis = { 1000.0, 80000.0 };
+  Double[] beef = { 1000.0, 90000.0 };
+  Double[] bawangBombay = { 500.0, 40000.0 };
+}
